0009d9293347e3b8e3d4d27b7f5cc4c2af2be4ea
rebecca-tinymouse <rebecca@tinymouse.io>
2020-11-20 22:03:08 +0000

    Remove unneeded micropython components

--- ports/stm32/boards/STM32L432KC_DEV/mpconfigboard.h
+++ ports/stm32/boards/STM32L432KC_DEV/mpconfigboard.h
@@ -20,6 +20,11 @@
 #define MICROPY_PY_UBINASCII        (0)
 #define MICROPY_PY_FRAMEBUF         (0)
 //#define MICROPY_PY_USELECT          (0)
+#define MICROPY_PY_ASYNC_AWAIT (0)
+#define MICROPY_PY_UASYNCIO (0)
+#define MICROPY_PY_UCTYPES (0)
+#define MICROPY_PY_URANDOM (0)
+#define MICROPY_PY_URANDOM_EXTRA_FUNCS (0)
 
 #define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE (1)
 #define MICROPY_HW_ENABLE_RTC       (0)
@@ -28,7 +33,18 @@
 #define MICROPY_HW_ENABLE_USB       (1) // using a custom USB connector on PA11/PA12
 #define MICROPY_HW_ENABLE_TIMER     (1)
 #define MICROPY_HW_HAS_SWITCH       (0)
-#define MICROPY_HW_HAS_FLASH        (1)
+#define MICROPY_HW_HAS_FLASH        (0) // Whether to expose internal flash storage as pyb.Flash
+
+#define MICROPY_PY_GC (0)
+//#define MICROPY_ENABLE_GC (0)
+
+#define MICROPY_CPYTHON_COMPAT (0)
+#define MICROPY_PY_ARRAY (0)
+
+#define MICROPY_PY_MATH (0)
+#define MICROPY_PY_COLLECTIONS (0)
+//#define MICROPY_PY_IO (0)
+//#define MICROPY_PY_SYS (0) breaks repl or something?
 
 // MSI is used and is 4MHz
 #define MICROPY_HW_CLK_PLLM (1)
@@ -43,36 +59,36 @@
 #define MICROPY_HW_RTC_USE_LSE      (0)
 
 // UART config
-#define MICROPY_HW_UART1_TX     (pin_B6)
-#define MICROPY_HW_UART1_RX     (pin_B7)
-#define MICROPY_HW_UART2_TX     (pin_A2)  // VCP TX
-#define MICROPY_HW_UART2_RX     (pin_A15) // VCP RX
+//#define MICROPY_HW_UART1_TX     (pin_B6)
+//#define MICROPY_HW_UART1_RX     (pin_B7)
+//#define MICROPY_HW_UART2_TX     (pin_A2)  // VCP TX
+//#define MICROPY_HW_UART2_RX     (pin_A15) // VCP RX
 
-#define MICROPY_HW_UART_REPL        PYB_UART_2
-#define MICROPY_HW_UART_REPL_BAUD   115200
+//#define MICROPY_HW_UART_REPL        PYB_UART_2
+//#define MICROPY_HW_UART_REPL_BAUD   115200
 
 #define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_4
 
-// I2C busses
-#define MICROPY_HW_I2C1_SCL (pin_A9)
-#define MICROPY_HW_I2C1_SDA (pin_A10)
-#define MICROPY_HW_I2C3_SCL (pin_A7)
-#define MICROPY_HW_I2C3_SDA (pin_B4)
+// I2C busses - not needed for current project
+//#define MICROPY_HW_I2C1_SCL (pin_A9)
+//#define MICROPY_HW_I2C1_SDA (pin_A10)
+//#define MICROPY_HW_I2C3_SCL (pin_A7)
+//#define MICROPY_HW_I2C3_SDA (pin_B4)
 
 // SPI busses
-#define MICROPY_HW_SPI1_NSS     (pin_B0)
-#define MICROPY_HW_SPI1_SCK     (pin_A5)
-#define MICROPY_HW_SPI1_MISO    (pin_A6)
-#define MICROPY_HW_SPI1_MOSI    (pin_A7)
-#define MICROPY_HW_SPI3_NSS     (pin_A4)
-#define MICROPY_HW_SPI3_SCK     (pin_B3)
-#define MICROPY_HW_SPI3_MISO    (pin_B4)
-#define MICROPY_HW_SPI3_MOSI    (pin_B5)
+//#define MICROPY_HW_SPI1_NSS     (pin_B0)
+//#define MICROPY_HW_SPI1_SCK     (pin_A5)
+//#define MICROPY_HW_SPI1_MISO    (pin_A6)
+//#define MICROPY_HW_SPI1_MOSI    (pin_A7)
+//#define MICROPY_HW_SPI3_NSS     (pin_A4)
+//#define MICROPY_HW_SPI3_SCK     (pin_B3)
+//#define MICROPY_HW_SPI3_MISO    (pin_B4)
+//#define MICROPY_HW_SPI3_MOSI    (pin_B5)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_B3) // Green LD3 LED on Nucleo
-#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
-#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
+//#define MICROPY_HW_LED1             (pin_B3) // Green LD3 LED on Nucleo
+//#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
+//#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
 
 // USB config
 #define MICROPY_HW_USB_FS           (MICROPY_HW_ENABLE_USB)
